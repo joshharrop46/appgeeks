526661d50d4ed1097bc0de15d2a655d56f103b36
joshharrop46 <joshharrop46@gmail.com>
2017-09-11 21:26:02 +0700

    versi 1

---
+++ wp-content/themes/MbaDin/search.php
@@ -0,0 +1,23 @@
+<?php
+if($_GET['s']!=''){
+$replace = array('+',' ');
+$urlredirect = get_settings('home') . '/gps/' . str_replace($replace,'-',$_GET['s']) . '/';
+header("HTTP/1.1 301 Moved Permanently");
+header( "Location: $urlredirect" );
+}
+?><?php get_header(); ?>
+<div style="clear: both"></div>
+<div id="nugede">
+<?php include (TEMPLATEPATH . '/sidebar-left.php'); ?>
+<div id="content"><div class="post"><div id="breadchumb"><?php if (function_exists('breadcrumbs')) breadcrumbs(); ?></div>
+<?php echo spp(get_search_query( '', false ), 'pdf.html', ' filetype:pdf');?>
+<input type="hidden" name="IL_IN_TAG" value="1"/>
+<?php echo spp(get_search_query( '', false ), 'imgamazon.html', ' filetype:pdf');?>
+
+<?php echo spp(get_search_query( '', false ), 'pdf2.html');?>  
+
+</div></div>	
+<div style="clear: both"></div>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
